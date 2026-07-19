258e277b3a53ca6a6af5f8dcac23b47db5712bd4
bemilbandin-ops <bemilbandin@gmail.com>
2026-07-19 18:05:42 +0200

    Enable pulse afterimage transform

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { sacredGeometryTransform } from './build/sacredGeometryTransform';
+import { afterimageTrailTransform } from './build/afterimageTrailTransform';
 
-export default defineConfig({ plugins: [sacredGeometryTransform(), react()] });
+export default defineConfig({
+  plugins: [sacredGeometryTransform(), afterimageTrailTransform(), react()],
+});
